9bf613e3cfa4373e866609386b8a7a0e3375d820
Manisha Karvannan <manishak@FACTENTRYUK.COM>
2023-08-01 15:16:42 +0530

    sample ts

---
+++ Sample.py
@@ -0,0 +1,20 @@
+import os
+import pandas as pd
+from pyspark.sql import SparkSession
+ 
+file_path = f"C:/Users/manishak/Downloads/11Jul2023.xlsx"
+df_content = pd.read_excel(file_path)
+df = df_content
+
+# Create PySpark SparkSession
+spark = SparkSession.builder \
+    .master("local[1]") \
+    .appName("Japan_table") \
+    .getOrCreate()
+#Create PySpark DataFrame from Pandas
+sparkDF=spark.createDataFrame(df_content) 
+sparkDF.printSchema()
+sparkDF.show()
+
+
+
