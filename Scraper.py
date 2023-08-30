95f75e4991d05c80d6a39093b66d5bbd2a09309b
Manisha Karvannan <manishakarvannan@gmail.com>
2023-08-30 15:59:58 +0530

    autoscraper

--- Scraper.py
+++ Scraper.py
@@ -1,6 +1,8 @@
 from autoscraper import AutoScraper
 import pandas as pd
-url = 'https://www.boerse-stuttgart.de/en/tools/finder-tools/most-frequent-trades/?category=4991'
+# url = 'https://www.boerse-stuttgart.de/en/tools/finder-tools/most-frequent-trades/?category=4991'
+
+url = 'https://pypi.org/project/autoflake/#description'
 
 wanted_list1 = ['Porsche Automobil Holding SE Medium Term Notes v.23(30/30)']
 wanted_list2 = ['97.611 G','1.756.199,00']
@@ -8,8 +10,14 @@ wanted_list3 = ['A351SX','/de-de/produkte/anleihen/stuttgart/a351sx-porsche-auto
 scraper = AutoScraper()
 #scraper.build(url, wanted_dict=wanted_dict)
 df = pd.DataFrame()
-df['Issuer'] = pd.DataFrame(scraper.build(url, wanted_list1))
-df['Price'] = pd.DataFrame(scraper.build(url, wanted_list2))
-df['Code'] = pd.DataFrame(scraper.build(url, wanted_list3))
+# df['Issuer'] = pd.DataFrame(scraper.build(url, wanted_list1))
+# df['Price'] = pd.DataFrame(scraper.build(url, wanted_list2))
+# df['Code'] = pd.DataFrame(scraper.build(url, wanted_list3))
+# wanted_list1 = ''
+# print(wanted_list1)
+# print(df)
+wanted_list4 = ['Projectdescription']
 
-print(df)
+wanted_list4 = ''
+result = scraper.build(url, wanted_list3)
+print(result)
