1a8c497c01ee41e3f2027accec739ed60bb4ce42
Manisha Karvannan <manishakarvannan@gmail.com>
2023-11-07 11:22:38 +0530

    ranker

---
+++ Rank.py
@@ -0,0 +1,28 @@
+###########  Regex Answer ###########
+
+
+# import re
+
+# Regex_Pattern = r'hackerrank'	# Do not delete 'r'.
+
+
+# Test_String = input()
+
+# match = re.findall(Regex_Pattern, Test_String)
+
+# print("Number of matches :", len(match))
+
+import textwrap
+
+
+def wrap(string, max_width):
+    wrapper = textwrap.TextWrapper(width=max_width) 
+    word_list = wrapper.wrap(text=string) 
+    for data in word_list:
+        print(data)
+    
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
+
