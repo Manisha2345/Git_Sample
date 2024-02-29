65767fb632fd0b72972f6fd0bd5dedc2c1c9964c
Manisha Karvannan <manishakarvannan@gmail.com>
2024-02-29 11:14:37 +0530

    hacker  rank

---
+++ hackerrank.py
@@ -0,0 +1,13 @@
+# https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
+
+
+### Feb -29-2024
+
+def solveMeFirst(num1,num2):
+    return (num1+num2)
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
